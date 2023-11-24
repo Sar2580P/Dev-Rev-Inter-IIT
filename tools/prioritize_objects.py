33e2963b41abc9c9361de9d3a4212f336fa27caf
Aditya Raj <109805994+zeus2x7@users.noreply.github.com>
2023-11-24 17:21:04 +0530

    Add files via upload

---
+++ tools/prioritize_objects.py
@@ -0,0 +1,30 @@
+from langchain.tools import BaseTool
+from typing import Optional, List, Any 
+from langchain.callbacks.manager import (
+    AsyncCallbackManagerForToolRun,
+    CallbackManagerForToolRun,
+)
+from backend_llm.utils import llm
+
+class Prioritize(BaseTool):
+    name = "Prioritize"
+    description = '''Returns a list of objects sorted by
+                priority. The logic of what constitutes priority for a given
+                object is an internal implementation detail.
+                '''
+    def _run(
+        self, query:str, run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        print('inside Prioritize_objects Tool , query is : \n' , query)
+        x = {
+            'argument_name': 'objects',
+            'argument_value': query,
+        }
+        return x 
+    
+
+    async def _arun(
+        self, query: str, run_manager: Optional[AsyncCallbackManagerForToolRun] = None
+    ) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("custom_search does not support async")
